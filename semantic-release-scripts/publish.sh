0f22850a56942a6ad73d329a18585811fa436051
Stone Sun <ssun@seismic.com>
2022-06-13 13:50:27 +0800

    fix: test exec plugin another

--- semantic-release-scripts/publish.sh
+++ semantic-release-scripts/publish.sh
@@ -2,11 +2,7 @@
 # Run rush or other cmd
 echo 'Cutomized CMD for Publishing...'
 echo "publishing $1 $2 $3"
-touch ./test-publish.log
 echo "publishing $1 $2 $3" >> ./test-publish.log
-cat ./test-publish.log
-pwd
-ls -la
-git add .
 # rush change
 # rush publish
+cat ./test-publish.log
